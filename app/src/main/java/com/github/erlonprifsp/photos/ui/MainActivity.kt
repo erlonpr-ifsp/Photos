207c0d8991e7192b2199e59725c5ac11d0418597
erlonprifsp <erlon.rodrigues@aluno.ifsp.edu.br>
2024-02-13 16:32:33 -0300

    feat(MainActivity): implementação do layout da MainActivity utilizando ViewBinding

--- app/src/main/java/com/github/erlonprifsp/photos/ui/MainActivity.kt
+++ app/src/main/java/com/github/erlonprifsp/photos/ui/MainActivity.kt
@@ -3,10 +3,22 @@ package com.github.erlonprifsp.photos.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.github.erlonprifsp.photos.R
+import com.github.erlonprifsp.photos.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity() { // classe MainActivity herda da classe AppCompatActivity
+
+    private val amb: ActivityMainBinding by lazy { // instancia a classe ActivityMainBinding gerada automaticamente pelo ViewBinding
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    // método onCreate
+    override fun onCreate(savedInstanceState: Bundle?) { // parâmetro savedInstanceState contém dados sobre o estado anterior da activity
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // setContentView(R.layout.activity_main)
+        setContentView(amb.root) // define o conteúdo da activity usando a raiz (root) do layout inflado por meio do ViewBinding
+
+        setSupportActionBar(amb.photosToolbar.apply { // define a toolbar como uma barra de ação da activity
+            title = getString(R.string.app_name) // define o título da toolbar com o mesmo nome do aplicativo
+        })
     }
 }
