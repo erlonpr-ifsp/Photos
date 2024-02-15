8d0fccfa3bfc9a36873acc028379436bb73ccd1d
erlonprifsp <erlon.rodrigues@aluno.ifsp.edu.br>
2024-02-15 19:34:09 -0300

    feat(photosSpinner): instancia o adapter na MainActivity e atribui ao adapter do spinner

--- app/src/main/java/com/github/erlonprifsp/photos/ui/MainActivity.kt
+++ app/src/main/java/com/github/erlonprifsp/photos/ui/MainActivity.kt
@@ -3,12 +3,19 @@ package com.github.erlonprifsp.photos.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.github.erlonprifsp.photos.R
+import com.github.erlonprifsp.photos.adapter.PhotoAdapter
 import com.github.erlonprifsp.photos.databinding.ActivityMainBinding
+import com.github.erlonprifsp.photos.model.PhotoListItem
 
 class MainActivity : AppCompatActivity() { // classe MainActivity herda da classe AppCompatActivity
 
     private val amb: ActivityMainBinding by lazy { // instancia a classe ActivityMainBinding gerada automaticamente pelo ViewBinding
         ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val photoList: MutableList<PhotoListItem> = mutableListOf() // data source
+    private val photoAdapter: PhotoAdapter by lazy { // adapter
+        PhotoAdapter(this, photoList)
     }
 
     // método onCreate
@@ -18,7 +25,12 @@ class MainActivity : AppCompatActivity() { // classe MainActivity herda da class
         setContentView(amb.root) // define o conteúdo da activity usando a raiz (root) do layout inflado por meio do ViewBinding
 
         setSupportActionBar(amb.photosToolbar.apply { // define a toolbar como uma barra de ação da activity
-            title = getString(R.string.app_name) // define o título da toolbar com o mesmo nome do aplicativo
+            title =
+                getString(R.string.app_name) // define o título da toolbar com o mesmo nome do aplicativo
         })
+
+        // spinner
+        amb.photosSpinner.adapter = photoAdapter
+
     }
 }
